73ab446608f2817b77421b28f2114a894171922e
Laffreuxbob <32509859+Laffreuxbob@users.noreply.github.com>
2018-06-27 10:08:41 +0200

    Calendar dragNdrop resizer

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // Force dangling commas on multiline declarations
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+}
